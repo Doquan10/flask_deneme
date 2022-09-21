efac9ed3df8da83fa8dc71d6c3576b558c0b67a4
Doğukan <57302274+Doquan10@users.noreply.github.com>
2022-09-21 15:24:59 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
-from flask import Flask,request,jsonify
-import numpy as np
-import pickle
-model = pickle.load(open('finalized_model.sav','rb'))
-app = Flask(__name__)
+from flask import Flask
+
+app= Flask(__name__)
+
 @app.route('/')
 def index():
-    return "Hello world"
-@app.route('/predict',methods=['POST'])
-def predict():
-    cgpa = request.form.get('cgpa')
-    iq = request.form.get('iq')
-    profile_score = request.form.get('profile_score')
-    input_query = np.array([[cgpa,iq,profile_score]])
-    result = model.predict(input_query)[0]
-    return jsonify({'placement':str(result)})
-if __name__ == '__main__':
-    app.run(debug=True)
+  return "<h1>Yazılım öğrenseydin...</h1>"
