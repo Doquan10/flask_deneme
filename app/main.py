0d48ab1aee6fcf19f6ec82fcfa353f3950f19afa
Doğukan <57302274+Doquan10@users.noreply.github.com>
2022-09-22 10:03:10 +0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello world"
+@app.route('/predict',methods=['POST'])    
+def predict():
+    cgpa = request.form.get('cgpa')
+    iq = request.form.get('iq')
+    profile_score = request.form.get('profile_score')
+    input_query = np.array([[cgpa,iq,profile_score]])
+    result = model.predict(input_query)[0]
+    return jsonify({'placement':str(result)})
+if __name__ == '__main__':
+    app.run(debug=True)
