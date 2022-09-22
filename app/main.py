0f84aaed3c8a97fba01d645e95dccd9f37809a89
Doğukan <57302274+Doquan10@users.noreply.github.com>
2022-09-22 16:11:18 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,17 +2,17 @@ from flask import Flask,request,jsonify
 import numpy as np
 import pickle
 
-model = pickle.load(open('model.sav','rb'))
+model = pickle.load(open('maliyet.pkl','rb'))
 app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello world"
 @app.route('/predict',methods=['POST'])    
 def predict():
-    cgpa = request.form.get('cgpa')
-    iq = request.form.get('iq')
-    profile_score = request.form.get('profile_score')
-    input_query = np.array([[cgpa,iq,profile_score]])
+    akma = request.form.get('akma')
+    cekme = request.form.get('cekme')
+    acinim = request.form.get('acinim')
+    input_query = np.array([[akma,cekme,acinim]])
     result = model.predict(input_query)[0]
     return jsonify({'placement':str(result)})
 if __name__ == '__main__':
