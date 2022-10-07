cc43d986ada6b984a2d96bf326cb9b03950bf3a2
Doğukan <57302274+Doquan10@users.noreply.github.com>
2022-10-07 15:34:29 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,24 +10,20 @@ def index():
 @app.route('/predict',methods=['POST'])    
 def predict():
     df = pd.DataFrame(columns=['OPERASYON SAYISI','KAMLI DELİK','TOPLAM AĞIRLIK',
-                           'KALIP TİPİ_0','KALIP TİPİ_1','KALIP TİPİ_2','KALIP TİPİ_3',
-                           'OEM_0','OEM_1','OEM_2','OEM_3'])
+                           'KALIP TİPİ_0','KALIP TİPİ_1','KALIP TİPİ_2','KALIP TİPİ_3'])
 
 
     operasyon_sayisi=request.form.get('akma')
     kamli_delik=request.form.get('cekme')
+    zorluk_katsayisi=2
     toplam_agirlik=request.form.get('acinim')
     kalip_tipi0=0
     kalip_tipi1=1
     kalip_tipi2=0
     kalip_tipi3=3
-    oem0=0
-    oem1=1
-    oem2=0
-    oem3=0
-    df2 = df.append({'OPERASYON SAYISI' : operasyon_sayisi, 'KAMLI DELİK' : kamli_delik, 'TOPLAM AĞIRLIK' : toplam_agirlik,
-                'KALIP TİPİ_0' : kalip_tipi0, 'KALIP TİPİ_1' : kalip_tipi1, 'KALIP TİPİ_2' : kalip_tipi2, 'KALIP TİPİ_3' : kalip_tipi3,
-                'OEM_0' : oem0, 'OEM_1' : oem1, 'OEM_2' : oem2, 'OEM_3' : oem3}, ignore_index = True)
+ 
+    df2 = df.append({'OPERASYON SAYISI' : operasyon_sayisi, 'KAMLI DELİK' : kamli_delik,'ZORLUK KATSAYISI'=zorluk_katsayisi, 'TOPLAM AĞIRLIK' : toplam_agirlik,
+                'KALIP TİPİ_0' : kalip_tipi0, 'KALIP TİPİ_1' : kalip_tipi1, 'KALIP TİPİ_2' : kalip_tipi2, 'KALIP TİPİ_3' : kalip_tipi3}, ignore_index = True)
 
 
 
